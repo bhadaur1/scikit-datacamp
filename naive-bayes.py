6af6e5993c45ccd3c3fe58d9f2c5f445da002fe5
Ravi Bhadauria <leoravs@gmail.com>
2019-03-10 11:57:54 -0500

    naive bayes code

---
+++ naive-bayes.py
@@ -0,0 +1,11 @@
+# Import scikit-learn dataset library
+from sklearn import datasets
+
+# Load dataset
+wine = datasets.load_wine()
+
+# print the names of the 13 features
+print("Features: ", wine.feature_names)
+
+# print the label type of wine(class_0, class_1, class_2)
+print("Labels: ", wine.target_names)
